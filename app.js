cd15003d98e211fdfdc8df2eabf66f0cade663de
bolheinmin <bolheinmin@gmail.com>
2020-01-26 14:02:27 +0630

    test

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ function handlePostback(sender_psid, received_postback) {
       }
       ]
     }
-  } else if (payload === 'Dota') {
+  } else if (payload === 'yes') {
     response = {
       "text":`What would you like to do?`,
       "quick_replies":[
@@ -258,9 +258,6 @@ function handlePostback(sender_psid, received_postback) {
   }
   else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
-  }
-  else if (payload === 'yes') {
-    response = { "text": "Ok"}
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
