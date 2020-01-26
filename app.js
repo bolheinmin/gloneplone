08370d274e8d9421f1749046f4f65c360808ff97
bolheinmin <bolheinmin@gmail.com>
2020-01-26 13:59:46 +0630

    fix

--- app.js
+++ app.js
@@ -259,6 +259,9 @@ function handlePostback(sender_psid, received_postback) {
   else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   }
+  else if (payload === 'yes') {
+    response = { "text": "Ok"}
+  }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
