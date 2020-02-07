03998c1baef27e3dd8e62e7ddee695a1d54af361
bolheinmin <bolheinmin@gmail.com>
2020-02-07 10:03:00 +0630

    get

--- app.js
+++ app.js
@@ -397,7 +397,7 @@ function callSendAPI(sender_psid, response) {
 
 function setupGetStartedButton(res){
         var messageData = {
-          "get_started":{"payload":"USER_DEFINED_PAYLOAD"}
+          "get_started":{"payload":"get_started"}
         };
         // Start the request
         request({
