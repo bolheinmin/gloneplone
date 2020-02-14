349a71c75c25597da48d95f11fc39b6ad2fddf57
bolheinmin <bolheinmin@gmail.com>
2020-02-14 11:21:35 +0630

    test

--- app.js
+++ app.js
@@ -163,6 +163,7 @@ function handleMessage(sender_psid, received_message) {
             "title":"Welcome!",
             "image_url":"https://petersfancybrownhats.com/company_image.png",
             "subtitle":"We have the right hat for everyone.",
+            "subtitle":"ok",
             "buttons":[
               {
                 "type":"web_url",
