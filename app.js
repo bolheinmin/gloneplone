149f30dbbb57a7203445f2c1676b0eda291fc557
bolheinmin <bolheinmin@gmail.com>
2020-02-14 10:01:22 +0630

    test

--- app.js
+++ app.js
@@ -309,24 +309,15 @@ function handlePostback(sender_psid, received_postback) {
         "template_type":"generic",
         "elements":[
            {
-            "title":"Welcome!",
+            "title":"Chicken",
             "image_url":"https://petersfancybrownhats.com/company_image.png",
             "subtitle":"We have the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://petersfancybrownhats.com/view?item=103",
-              "webview_height_ratio": "tall",
-            },
             "buttons":[
               {
                 "type":"web_url",
                 "url":"https://petersfancybrownhats.com",
-                "title":"View Website"
-              },{
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-              }              
+                "title":"Choose"
+              }            
             ]      
           },
           {
